f5c82ef2a42b3f040a83aea46e43b25aab5eb6f6
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-07 01:47:26 +0900

    軽微な修正

--- Omuct Fes 3D/Assets/Effect.cs
+++ Omuct Fes 3D/Assets/Effect.cs
@@ -1,5 +1,5 @@
 abstract public class Effect{
     public int time;
     public int type;
-    abstract void Tick(Player player);
+    protected abstract void Tick(Player player);
 }
--- Omuct Fes 3D/Assets/Item.cs
+++ Omuct Fes 3D/Assets/Item.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 abstract public class Item{
     public Texture2D itemImage;
     abstract public void Use(Player user);
