427d2bf88721c0712ba10bee68a7df4327407511
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-08 02:22:22 +0900

    create ekipu

---
+++ Omuct Fes 3D/Assets/Ekipu/PlayerEkipu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+public class PlayerEkipu : Player{
+    public GameObject attackObject;
+    private long lastAttackTime=0;
+    override protected void Attack(){
+        long time=DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + 
+    DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
+        if(time<lastAttackTime+1000)
+            return;
+        lastAttackTime=time;
+        GameObject cloneObject=Instantiate(attackObject,transform.position-cameraVec3*2,Quaternion.identity);
+        Rigidbody rb=cloneObject.GetComponent<Rigidbody>();
+        rb.AddForce(this.cameraVec3*-1000);
+    }
+}
