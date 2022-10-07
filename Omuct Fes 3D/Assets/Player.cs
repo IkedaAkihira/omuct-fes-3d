a3cf10450880eb2d0adece70f34fe06a2a00591b
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-08 02:21:58 +0900

    update player

--- Omuct Fes 3D/Assets/Player.cs
+++ Omuct Fes 3D/Assets/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+abstract public class Player : MonoBehaviour
 {
     public int id;
     protected int hp;
@@ -24,30 +24,27 @@ public class Player : MonoBehaviour
     private float cameraRotationY = 0f;
     private Vector3 move;
     private Vector3 cameraVec2;
+    public Vector3 cameraVec3;
 
-    public GameObject camera;
+    public GameObject tpsCamera;
     CameraMover cameraMover;
     public float cameraDistance = 10.0f;
     private void Start()
     {
         controller = this.GetComponent<CharacterController>();
-        cameraMover=this.camera.GetComponent<CameraMover>();
+        cameraMover=tpsCamera.GetComponent<CameraMover>();
         move=new Vector3(0,0,0);
         cameraVec2=new Vector3(0,0,0);
+        cameraVec3=new Vector3(0,0,0);
     }
 
     void Update()
     {
-        //camera
+        //camera rotation
         cameraRotation=-Input.mousePosition.x*0.005f;
-
         cameraRotationY=Mathf.Min(Mathf.Max(-1.0f,(-Input.mousePosition.y)*0.01f),1.0f);
 
-        
-
-
-
-
+        //move
         move = -cameraVec2*Input.GetAxis("Vertical")
         +new Vector3(
             -Mathf.Sin(cameraRotation),
@@ -58,37 +55,54 @@ public class Player : MonoBehaviour
         {
             gameObject.transform.forward = move;
         }
+
+        //jump
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += jumpForce;
         }
-
         
+        //attack
+        if(Input.GetKey(KeyCode.E)){
+            AttackEvent e=new AttackEvent(this);
+            GameMaster.instance.OnAttack(e);
+            if(e.isAvailable){
+                Attack();
+            }
+        }
+
+        //use item
+        if(Input.GetKey(KeyCode.Q)){
+            if(item!=null){
+                item.Use(this);
+                item=null;
+            }
+        }
     }
 
     private void FixedUpdate() {
+        //update camera
         cameraVec2=new Vector3(
             Mathf.Cos(cameraRotation),
             0,
             Mathf.Sin(cameraRotation)
             );
-
-        Vector3 cameraVec3=new Vector3(0,Mathf.Sin(cameraRotationY),0)+cameraVec2*Mathf.Cos(cameraRotationY);
+        cameraVec3=new Vector3(0,Mathf.Sin(cameraRotationY),0)+cameraVec2*Mathf.Cos(cameraRotationY);
         cameraMover.MoveCamera(this.transform.position,cameraVec3,cameraDistance);
         
+        //gravity
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && (playerVelocity.y < 0))
         {
             playerVelocity.y = 0f;
         }
         playerVelocity.y += gravityValue * Time.deltaTime;
+
+        //move
         controller.Move((move*playerSpeed+playerVelocity) * Time.deltaTime);
     }
     
-    protected void Attack(){
-        AttackEvent e=new AttackEvent(this);
-        GameMaster.instance.OnAttack(e);
-    }
+    abstract protected void Attack();
     public void Damage(DamageSource damageSource){
         DamageEvent e=new DamageEvent(this,damageSource);
         GameMaster.instance.OnDamaged(e);
