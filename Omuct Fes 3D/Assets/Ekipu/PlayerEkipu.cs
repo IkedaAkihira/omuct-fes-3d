f38bbd10cd218648e71152f43f39d18410447f53
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-08 15:48:37 +0900

    Ekipuの軽微な修正

--- Omuct Fes 3D/Assets/Ekipu/PlayerEkipu.cs
+++ Omuct Fes 3D/Assets/Ekipu/PlayerEkipu.cs
@@ -4,22 +4,27 @@ using System;
 using UnityEngine;
 public class PlayerEkipu : Player{
     public GameObject attackObject;
+    public float attackForce=1000f;
+    public int attackInterval=200;
     private long lastAttackTime=0;
     override protected void Attack(){
         long time=DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + 
     DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
-        if(time<lastAttackTime+1000)
+        if(time<lastAttackTime+attackInterval)
             return;
         lastAttackTime=time;
         RaycastHit hit;
         Vector3 attackVec;
-        if(Physics.Raycast(transform.position+new Vector3(0f,1f,0f),cameraVec3,out hit,Mathf.Infinity)){
-            attackVec=(hit.point-(transform.position+new Vector3(0f,1f,0f))).normalized;
+        if(
+            Physics.Raycast(transform.position+new Vector3(0f,1f,0f),cameraVec3,out hit,Mathf.Infinity)
+            &&hit.collider.GetComponent<PlayerEkipu>()!=this
+        ){
+            attackVec=(hit.point-transform.position).normalized;
         }else{
             attackVec=cameraVec3.normalized;
         }
         GameObject cloneObject=Instantiate(attackObject,transform.position+attackVec*2,Quaternion.identity);
         Rigidbody rb=cloneObject.GetComponent<Rigidbody>();
-        rb.AddForce(attackVec*1000);
+        rb.AddForce(attackVec*attackForce);
     }
 }
