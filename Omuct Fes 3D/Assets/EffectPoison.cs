aa091fe7c56363264610f9ea9e4e33b29fe578a7
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-14 01:12:26 +0900

    add poison effect

--- Omuct Fes 3D/Assets/EffectPoison.cs
+++ Omuct Fes 3D/Assets/EffectPoison.cs
@@ -1,23 +1,33 @@
+using UnityEngine;
+
 public class EffectPoison : Effect{
     //あとで調整できるように、ダメージを受ける間隔と毎回のダメージを指定している。今回は1damage/sec * 10sec = 10damage
     int interval=50;
     int damage=1;
 
+    private ParticleSystem poisonParticle;
+
     //コンストラクタ　new EffectPoison(...)をやったときの処理を書いてある。
     public EffectPoison(int time)
     {
         this.time=time;
+        this.poisonParticle=Resources.Load<ParticleSystem>("Prefabs/PoisonEffect");
     }
 
     //毎秒50回呼ばれる。
     override public void Tick(Player player)
     {
         //ゲーム内時間をintervalで割ったあまりが0の時、つまり、interval回ごとにプレイヤーにダメージを与える。
-        if(GameMaster.instance.gameTime%interval==0)
+        if(time%interval==interval-5)
         {
             //player.Damage(damageSource)でプレイヤーにダメージを与えられる。
             //damageSourceはダメージの情報を保持してる。
             player.Damage(new DamageSource(damage));
+        }else if(time%interval==0)
+        {
+
+            ParticleSystem clone=GameObject.Instantiate(poisonParticle);
+            clone.transform.position=player.transform.position;
         }
     }
 }
