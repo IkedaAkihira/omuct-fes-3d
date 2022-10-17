8bd1b0656d526cdac7575e6bb95a9af5d70cf443
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-18 00:55:13 +0900

    gear attack

--- Omuct Fes 3D/Assets/Ekipu/BulletEkipu.cs
+++ Omuct Fes 3D/Assets/Ekipu/BulletEkipu.cs
@@ -1,32 +1,37 @@
 using UnityEngine;
 
 public class BulletEkipu : MonoBehaviour {
-    public GameObject explosion;
     public Player parent;
 
     long startTime;
 
+    Rigidbody rb;
+
     private void Start() {
-        startTime=GameMaster.instance.gameTime;
+        this.rb=this.GetComponent<Rigidbody>();
+        this.startTime=GameMaster.instance.gameTime;
     }
 
     private void FixedUpdate() {
         if(GameMaster.instance.gameTime-startTime>100)
-            Bomb();
+            Destroy(this.gameObject);
+    }
+
+    private void Update() {
+        this.transform.Rotate( 0f, 720.0f * Time.deltaTime ,0f );
     }
     private void OnTriggerEnter(Collider other) {
         if(other.isTrigger)
             return;
         Player p=other.GetComponent<Player>();
-        if(p!=null&&p==parent)
+        if(p==this.parent)
             return;
-        Bomb();
-    }
+        
 
-    private void Bomb(){
-        Destroy(gameObject);
-        ExplosionEkipu cloneExplosion=Instantiate(explosion,transform.position,Quaternion.identity)
-        .GetComponent<ExplosionEkipu>();
-        cloneExplosion.parent=parent;
+        if(p!=null)
+            p.Damage(new DamageSource(2));
+        
+        Destroy(this.gameObject);
+
     }
 }
