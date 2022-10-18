6a6091e5304f3d7c2c65579bee143ad832028345
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-19 01:01:11 +0900

    攻撃の出所を調整

--- Omuct Fes 3D/Assets/Ekipu/PlayerEkipu.cs
+++ Omuct Fes 3D/Assets/Ekipu/PlayerEkipu.cs
@@ -13,7 +13,7 @@ public class PlayerEkipu : Player{
             return;
         lastAttackTime=GameMaster.instance.gameTime;
         
-        GameObject cloneObject=Instantiate(attackObject,transform.position+new Vector3(0f,0.5f,0f),Quaternion.identity);
+        GameObject cloneObject=Instantiate(attackObject,transform.position+new Vector3(0f,0f,0f),Quaternion.identity);
         Rigidbody rb=cloneObject.GetComponent<Rigidbody>();
         rb.AddForce(toTargetVec*attackForce);
         BulletEkipu bullet=cloneObject.GetComponent<BulletEkipu>();
