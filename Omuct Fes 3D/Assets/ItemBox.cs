1592df86faf915c232d9caba453d0539ccfc28d3
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-18 17:40:18 +0900

    fix itemBox

--- Omuct Fes 3D/Assets/ItemBox.cs
+++ Omuct Fes 3D/Assets/ItemBox.cs
@@ -6,11 +6,12 @@ using System.Collections.Generic;
 public class ItemBox : MonoBehaviour {
     List<Item>  items=new List<Item>();
     
-    void Start()
+    void Awake()
     {
         //ここに作成したItemクラスを追加していく。
         //テスト時は不要なものをコメントアウトすると良い。
         items.Add(new ItemPoison());
+        items.Add(new ItemShootingBit());
     }
     private void Update() {
         transform.Rotate( 0f, 120.0f * Time.deltaTime ,0f );
@@ -23,7 +24,8 @@ public class ItemBox : MonoBehaviour {
             return;
         
         System.Random random=new System.Random();
-        player.item=items[random.Next(0,items.Count)];
+        int randomIndex=random.Next(0,items.Count);
+        player.item=items[randomIndex];
         Destroy(gameObject);
     }
 }
