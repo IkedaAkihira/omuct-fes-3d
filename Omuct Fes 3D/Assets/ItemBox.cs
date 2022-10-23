7e1fc018eede983e82a5e478e0acb45b47b1e936
Nachia Vivias <piyopiyograph@gol.com>
2022-10-23 19:45:38 +0900

    add an item ItemTokeito

--- Omuct Fes 3D/Assets/ItemBox.cs
+++ Omuct Fes 3D/Assets/ItemBox.cs
@@ -11,6 +11,7 @@ public class ItemBox : MonoBehaviour {
         //ここに作成したItemクラスを追加していく。
         //テスト時は不要なものをコメントアウトすると良い。
         items.Add(new ItemPoison());
+        items.Add(new ItemTokeito());
     }
     private void Update() {
         transform.Rotate( 0f, 120.0f * Time.deltaTime ,0f );
