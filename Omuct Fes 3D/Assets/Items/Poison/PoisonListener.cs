6f68a34bfccb5789c16c3f3572722c9e2ff7a620
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-26 21:23:04 +0900

    add speed down and jump power down on poison

---
+++ Omuct Fes 3D/Assets/Items/Poison/PoisonListener.cs
@@ -0,0 +1,19 @@
+public class PoisonListener: EventListener{
+    public void OnMove(MoveEvent e){
+        if(e.player.effects.ContainsKey(EffectPoison.TYPE))
+            e.Multiply(0.4f);
+    }
+    public void OnAttack(AttackEvent e){
+
+    }
+    public void OnDamaged(DamageEvent e){
+
+    }
+    public void OnUseItem(UseItemEvent e){
+
+    }
+    public void OnJump(JumpEvent e){
+        if(e.player.effects.ContainsKey(EffectPoison.TYPE))
+            e.Multiply(0.7f);
+    }
+}
