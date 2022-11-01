a27d53c8abdc5f596dccaba681593245c73088d0
Nachia Vivias <piyopiyograph@gol.com>
2022-11-02 01:54:50 +0900

    revert dev changes

--- Omuct Fes 3D/Assets/DataTransfer.cs
+++ Omuct Fes 3D/Assets/DataTransfer.cs
@@ -1,6 +1,6 @@
 public class DataTransfer{
-    static public long player1CharacterNumber = 2;
-    static public long player2CharacterNumber = 2;
+    static public long player1CharacterNumber;
+    static public long player2CharacterNumber;
 
     static public ResultData player1ResultData;
     static public ResultData player2ResultData;
