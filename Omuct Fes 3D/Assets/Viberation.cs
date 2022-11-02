d188e8e09500a48146049fc9cbd8e7593e2cea2b
Ikeda Akihira <91947939+IkedaAkihira@users.noreply.github.com>
2022-11-02 11:12:47 +0900

    Add Viberation.cs

---
+++ Omuct Fes 3D/Assets/Viberation.cs
@@ -0,0 +1,8 @@
+public class Viberation{
+    public int time;
+    public int magnitude;
+    public Viberation(int time,int magnitude){
+        this.time = time;
+        this.magnitude = magnitude;
+    }
+}
