6f3c9439f34905f8877d10566439270b5f727b4d
Ikeda Akihira <91947939+IkedaAkihira@users.noreply.github.com>
2022-11-02 11:35:47 +0900

    Update Viberation.cs

--- Omuct Fes 3D/Assets/Viberation.cs
+++ Omuct Fes 3D/Assets/Viberation.cs
@@ -1,8 +1,15 @@
+using UnityEngine;
+
 public class Viberation{
     public int time;
-    public int magnitude;
-    public Viberation(int time,int magnitude){
+    public float magnitude;
+    public Viberation(int time,float magnitude){
         this.time = time;
         this.magnitude = magnitude;
+    }
+    
+    public Vector2 GetVibeVec(){
+        Unity Random rand = new Unity.Random();
+        return new Vector2(rand.NextFloat()*this.magnitude,rand.NextFloat()*this.magnitude);
     }
 }
