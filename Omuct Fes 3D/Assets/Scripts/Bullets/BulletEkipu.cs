c5d278541ee61b4ef3420c38791dbc67e45ec96d
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-11-09 19:39:46 +0900

    gearを独立させた これで誰でもエキプの攻撃ができる

--- Omuct Fes 3D/Assets/Scripts/Bullets/BulletEkipu.cs
+++ Omuct Fes 3D/Assets/Scripts/Bullets/BulletEkipu.cs
@@ -6,9 +6,20 @@ public class BulletEkipu : Bullet {
 
     public float defaultSize = 1f;
 
-    public Quaternion targetRotation;
-    public PlayerEkipu parentEkipu;
-    public int splitRemain = 1;
+    Quaternion targetRotation;
+    [SerializeField] int splitRemain = 1;
+
+    [SerializeField] int splitInterval = 30;
+    [SerializeField] Vector2 attackVibration = new Vector2(1.5f,1.5f);
+    [SerializeField] float attackWidthAngle = 12f;
+
+    float attackForce = 3000f;
+
+    private void Start() {
+        if(parent.attacker is AttackerThrow)
+            this.attackForce = ((AttackerThrow)parent.attacker).bulletForce;
+        this.targetRotation = parent.TargetVecAsQuaternion;
+    }
 
     private void Update() {
         transform.localScale = new Vector3(1f,1f,1f)*(defaultSize+(float)(GameMaster.instance.gameTime-startTime)*magnification);
@@ -16,13 +27,13 @@ public class BulletEkipu : Bullet {
         this.transform.Rotate( 0f, 720.0f * Time.deltaTime ,0f );
         if(
             splitRemain > 0 &&
-            GameMaster.instance.gameTime - startTime >= parentEkipu.splitInterval
+            GameMaster.instance.gameTime - startTime >= splitInterval
         ){
             Quaternion[] rotations = new Quaternion[3];
             rotations[0] = Quaternion.Euler(
                 0.0f,
-                parentEkipu.attackWidthAngle + parentEkipu.attackVibration.x * (Random.value * 2.0f - 1.0f), // horizontal
-                parentEkipu.attackVibration.y * (Random.value * 2.0f - 1.0f) // vertical
+                attackWidthAngle + attackVibration.x * (Random.value * 2.0f - 1.0f), // horizontal
+                attackVibration.y * (Random.value * 2.0f - 1.0f) // vertical
             );
             rotations[1] = Quaternion.identity;
             rotations[2] = Quaternion.Inverse(rotations[0]);
@@ -37,12 +48,11 @@ public class BulletEkipu : Bullet {
                 // launch way
                 Vector3 way = rot * new Vector3(-1f, 0f, 0f);
 
-                GameObject cloneObject = Instantiate(parentEkipu.attackObject, transform.position + new Vector3(0f, 0f, 0f), Quaternion.identity);
+                GameObject cloneObject = Instantiate(this.gameObject, transform.position + new Vector3(0f, 0f, 0f), Quaternion.identity);
                 Rigidbody rb = cloneObject.GetComponent<Rigidbody>();
-                rb.AddForce(way * parentEkipu.attackForce);
+                rb.AddForce(way * attackForce);
                 BulletEkipu bullet = cloneObject.GetComponent<BulletEkipu>();
                 bullet.parent = this.parent;
-                bullet.parentEkipu = this.parentEkipu;
                 bullet.splitRemain = this.splitRemain - 1;
             }
             Destroy(this.gameObject);
