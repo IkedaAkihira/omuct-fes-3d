1637a494ee5b99903aab4f5154c83e83257ea22b
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-11-09 22:41:19 +0900

    チンアナゴの攻撃が自立して回るように

--- Omuct Fes 3D/Assets/Scripts/Bullets/BulletChinanago.cs
+++ Omuct Fes 3D/Assets/Scripts/Bullets/BulletChinanago.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class BulletChinanago : Bullet {
     [SerializeField] int damage = 2;
 
+    private void Start() {
+        transform.forward = rb.velocity;
+    }
+
     private void Update() {
         this.transform.Rotate( 720.0f * Time.deltaTime, 0f ,0f );
     }
